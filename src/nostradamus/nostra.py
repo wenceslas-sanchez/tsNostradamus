a7e4377de5df895ea7b57f487b45cf9103429653
Orlogskapten <wenceslas.sanchez@gmail.com>
2020-07-05 18:24:44 +0200

    Managed error for tunnelSnake

--- src/nostradamus/nostra.py
+++ src/nostradamus/nostra.py
@@ -1,7 +1,15 @@
 import numpy as np
 from statsmodels.tsa.arima_model import ARIMA
 
-def tunnelSnake(serie, shift, threshold):
+
+def exception_type(arg, typed):
+    if isinstance(arg, typed):
+        pass
+    else:
+        raise Exception("Wrong Type")
+
+
+def tunnelSnake(serie, shift, threshold, verbose= True):
     """
 
     :param serie: array
@@ -9,8 +17,15 @@ def tunnelSnake(serie, shift, threshold):
     :param threshold: float, define the level
     :return: array, serie without outlier values
     """
-    assert type(shift) == int
-    assert len(serie) != 0
+
+    # Control type
+    exception_type(serie, (list, tuple, np.ndarray))
+    exception_type(threshold, (int, float))
+    exception_type(shift, int)
+    exception_type(verbose, bool)
+
+    assert len(serie) > shift
+
 
     def moving_average(serie, shift):
         cumsum_array= np.cumsum(serie)
@@ -20,7 +35,7 @@ def tunnelSnake(serie, shift, threshold):
 
     def augmented_borne_ma(serie, shift, threshold):
         moving_average_serie= moving_average(serie, shift)
-        moving_average_serie= np.append(serie[:-shift-1], moving_average_serie[:-1])
+        moving_average_serie= np.append(serie[0:shift], moving_average_serie[:-1])
 
         up_aug_ma= moving_average_serie.copy()
         up_aug_ma[shift:]= up_aug_ma[shift:] * (1 + threshold)
@@ -33,19 +48,22 @@ def tunnelSnake(serie, shift, threshold):
     # Control serie type and serie content type
     serie= np.array(serie).astype(float)
 
-    print(augmented_borne_ma(serie, shift, threshold))
     up_aug_ma, down_aug_ma= augmented_borne_ma(serie, shift, threshold)
 
-    print(serie)
+    # Which values are sup / inf to the augmented MA
+    # and get the augmented MA value when it's True
     boolean_up = serie >= up_aug_ma
     boolean_down = serie < down_aug_ma
-
     boolean_serie = boolean_up * up_aug_ma + boolean_down * down_aug_ma
+
+    # Get the index of values we have to change
     index_to_change = (boolean_serie != 0).nonzero()
 
-    print(boolean_up)
-    print(boolean_down)
+    if verbose:
+        # We removed the shift first values, because they are always into index_to_change
+        print("Values at place {} were changed.".format(index_to_change[0][shift:]))
 
+    # Replace from inital array
     treated_serie = serie.copy()
     treated_serie[index_to_change[0]] = boolean_serie[index_to_change[0]]
 
