eb9635298bf664c21e4922f4ed2254100e8ae0d4
Orlogskapten <wenceslas.sanchez@gmail.com>
2020-07-07 21:21:39 +0200

    Tested new structure

--- test/class_implementation_test.py
+++ test/class_implementation_test.py
@@ -1,4 +1,28 @@
-import nostradamus.nostra as ns
+import pandas as pd
+import numpy as np
 
-ns.BasicNostra()
+import nostradamus.preprocessing as ns_preprocessing
 
+data= pd.read_excel(r"superstore.xls")
+
+# Get information
+furniture= data[data['Category'] == 'Furniture']
+furniture= furniture[["Order Date", "Sales"]]
+
+# Transform into Datetime object
+furniture["Order Date"]= pd.to_datetime(furniture["Order Date"], format= "%Y-%m-%d")
+# Order by date
+furniture= furniture.sort_values(["Order Date"]).reset_index(drop= True)
+# Index
+furniture= furniture.set_index("Order Date")
+
+# Sales mean / max / min per month (3 series)
+furniture_mean= furniture["Sales"].resample('MS').mean().values
+furniture_max= furniture["Sales"].resample('MS').max().values
+furniture_min= furniture["Sales"].resample('MS').min().values
+
+
+
+test= ns_preprocessing.tunnelSnake(furniture_mean, 3, 0.5)
+print(test.fit_transform())
+test.plot()
