a7171a16afd5cdfbd6ae2b96a7948eb6d48ba059
Wenceslas Sanchez <51153777+Orlogskapten@users.noreply.github.com>
2020-07-11 13:14:22 +0200

    Adding checking function for whole number

--- src/nostradamus/utils/error.py
+++ src/nostradamus/utils/error.py
@@ -3,9 +3,21 @@ Error management
 '''
 
 
-def exception_type(arg, typed):
+def exception_type(arg, typed) -> None:
     if isinstance(arg, typed):
         pass
     else:
         raise Exception("Wrong Type")
     pass
+
+def check_is_int(arg) -> None:
+
+    def is_integer(n) -> bool:
+        return n % 1 == 0
+
+    if is_integer(arg):
+        pass
+    else:
+        raise ValueError("Only integers are allowed")
+    pass
+
