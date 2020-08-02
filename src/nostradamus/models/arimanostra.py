2f031ddf47db47f44104ead477c56cd1a349a8fb
Wenceslas Sanchez <51153777+Orlogskapten@users.noreply.github.com>
2020-08-02 15:32:28 +0200

    Added plot diagnostic function

--- src/nostradamus/models/arimanostra.py
+++ src/nostradamus/models/arimanostra.py
@@ -2,17 +2,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MaxNLocator
 import seaborn as sns
+
 sns.set()
 import warnings
+
 warnings.filterwarnings("ignore")
 
 import scipy
 from scipy.stats import skewnorm
+from scipy import stats
 from scipy.optimize import brute
 
 from typing import Any, Iterable, List, Dict, Tuple
 
 from statsmodels.tsa.arima_model import ARIMA
+from statsmodels.api import add_constant
+from statsmodels import regression
+from statsmodels.graphics.tsaplots import plot_acf
 
 from ..utils.error import (exception_type
 , check_is_int
@@ -199,12 +205,16 @@ class ArimaNostra:
         # error for each model
 
         error_list = []
+        real_and_forecast = []
         for i in range(self.num_forecasting - self.forecast_range):
             pred_t1 = self.fit_[i]["forecast"]
             real_values = self.serie[self.train_len + i: self.train_len + self.forecast_range + i]
             error_list.append(pred_t1 - real_values)
+            real_and_forecast.append((real_values, pred_t1))
 
         self.error_by_models = np.array(error_list)
+        self.real_and_forcecast = np.array(real_and_forecast)
+
         return self.error_by_models
 
     def fit(self) -> List[Dict[str, Any]]:
@@ -291,11 +301,16 @@ class ArimaNostra:
         plt.show()
         pass
 
-    def plot_error_by_period(self, figsize: Tuple = (8, 5), error_dist: str = None, bins: int = 10) -> None:
+    def __type_error_matrix_period(self, error_type: str = "mae") -> np.ndarray:
+        if error_type == "mae":
+            return np.abs(self.error_by_models).T
+        else:
+            raise ValueError("Only MAE accepted")
 
-        error_type = "mae"
-        if self.error_by_periods is None:
-            self.error(error_type)
+    def plot_error_by_period(self, figsize: Tuple = (8, 5), error_dist: str = None, bins: int = 10
+                             , error_type: str = "mae") -> None:
+
+        self.error(error_type)  # not mae nor rmse because can't use mean ofr hist or boxplot
 
         if error_dist is None:
             error_dist = "no"
@@ -303,8 +318,9 @@ class ArimaNostra:
         ax = plt.figure(figsize=figsize).gca()
 
         # hist
+        error_matrix_period = self.__type_error_matrix_period(error_type)
+
         if error_dist == "hist":
-            error_matrix_period = np.abs(self.error_by_models).T
 
             for x in range(self.forecast_range):
                 weights = np.ones_like(error_matrix_period[x]) / len(error_matrix_period[x])
@@ -314,12 +330,13 @@ class ArimaNostra:
             plt.xlim((-0.1, self.forecast_range))
 
         elif error_dist == "boxplot":
-            error_matrix_period = np.abs(self.error_by_models).T
+
             error_matrix_period_list = []
             for x in range(self.forecast_range):
                 error_matrix_period_list.append(error_matrix_period[x])
 
             ax.boxplot(error_matrix_period_list, positions=np.arange(0, self.forecast_range, 1).astype(int))
+
 
         elif error_dist == "no":
             pass
@@ -350,34 +367,62 @@ class ArimaNostra:
 
         pass
 
+    def __alpha_beta_coef(self, x: np.ndarray, y: np.ndarray) -> Tuple[float, float]:
+        x = add_constant(x)
+        model = regression.linear_model.OLS(y, x).fit()
+
+        alpha, beta = model.params[0], model.params[1]
+        return alpha, beta
+
     def plot_diagnostic(self, bins: int = 25) -> None:
         # error
         residuals = self.error_by_models.flatten()
         norm_residuals = (residuals - np.mean(residuals)) / np.std(residuals)
+        flatten_all_real = np.array([self.real_and_forcecast[i][0] for i in range(self.num_forecasting \
+                                                                                  - self.forecast_range)]).flatten()
 
-        fig, ax = plt.subplots(nrows=2, ncols=1, figsize=(15, 10))
+        flatten_all_forecast_error = np.array([self.real_and_forcecast[i][1] - self.real_and_forcecast[i][0]
+                                               for i in range(self.num_forecasting - self.forecast_range)]).flatten()
+
+        fig, ax = plt.subplots(nrows=2, ncols=2, figsize=(15, 10))
+        fig.subplots_adjust(hspace= 0.5)
 
         # Histogram
         mu, std = scipy.stats.norm.fit(residuals)
 
-        ax[0].hist(residuals, bins=bins, density=True, alpha=0.6, color='purple', label="Données")
+        ax[0, 0].hist(residuals, bins=bins, density=True, alpha=0.6, color='purple', label="Données")
 
         # Plot le PDF.
-        xmin, xmax = ax[0].get_xlim()
+        xmin, xmax = ax[0, 0].get_xlim()
         X = np.linspace(xmin, xmax)
 
-        ax[0].plot(X, scipy.stats.norm.pdf(X, mu, std), label="Normal Distribution")
-        ax[0].plot(X, skewnorm.pdf(X, *skewnorm.fit(residuals)), color='black', label="Skewed Normal Distribution")
+        ax[0, 0].plot(X, scipy.stats.norm.pdf(X, mu, std), label="Normal Distribution")
+        ax[0, 0].plot(X, skewnorm.pdf(X, *skewnorm.fit(residuals)), color='black', label="Skewed Normal Distribution")
 
         mu, std = scipy.stats.norm.fit(residuals)
-        sk = scipy.stats.skew(residuals)[0]  # erreur possible avec l'ajout du [0]
+        sk = scipy.stats.skew(residuals)
 
         title2 = "Moments mu: {}, sig: {}, sk: {}".format(round(mu, 4), round(std, 4), round(sk, 4))
-        ax[0].set_ylabel("Fréquence", rotation=90)
-        ax[0].set_title(title2)
-        ax[0].legend()
+        ax[0, 0].set_ylabel("Fréquence", rotation=90)
+        ax[0, 0].set_title(title2)
+        ax[0, 0].legend()
+
+        # OLS
+        alpha, beta= self.__alpha_beta_coef(x= flatten_all_forecast_error, y= flatten_all_real)
+
+        ax[0, 1].scatter(y = flatten_all_real, x = flatten_all_forecast_error)
+        ax[0, 1].plot(flatten_all_forecast_error, alpha + flatten_all_forecast_error * beta, color="red")
+        #ax[0, 1].grid()
+        ax[0, 1].set_ylabel("Observations")
+        ax[0, 1].set_xlabel("Forecast error")
+        ax[0, 1].set_title("OLS Obs vs error")
+
+        # autocorr
+        plot_acf(flatten_all_forecast_error, lags= self.forecast_range, ax= ax[1, 0])
 
         # qqplot
+        stats.probplot(residuals, plot=plt)
 
         plt.show()
+
         pass
