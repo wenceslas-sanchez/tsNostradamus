5d82db3e89720a4ed0c3ae3f633ee6803695c4da
Wenceslas Sanchez <51153777+Orlogskapten@users.noreply.github.com>
2020-08-05 09:30:06 +0200

    Added basic methods (like ArimaNostra structure)

--- src/nostradamus/models/sarimanostra.py
+++ src/nostradamus/models/sarimanostra.py
@@ -1,9 +1,23 @@
 import numpy as np
 from statsmodels.tsa.statespace.sarimax import SARIMAX
 
+from ..models.mother import ArimaMother
 
-class SarimaNostra():
+class SarimaNostra(ArimaMother):
 
-    def __init__(self):
+    def __init__(self, serie, max_order_set, train_len, forecast_range
+                 , walk_forward=True, alpha=0.05, metric="aic", verbose=True
+                 , enforce_complexity=None):
+        super().__init__(serie, max_order_set, train_len, forecast_range
+                         , walk_forward=walk_forward, alpha=alpha, metric=metric, verbose=verbose)
+
         pass
 
+    def __fit(self):
+        pass
+
+    def __fit_optim_sarima(self):
+        pass
+
+    def search_for_the_goodone(self):
+        pass
